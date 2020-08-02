34ef03ef3f143989bfe4922dc3c5d90c904f4e69
Minecodes <65566012+Minecodes@users.noreply.github.com>
2020-08-02 10:52:08 +0200

    Install file and the script.

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+echo "PineapplePi by Minecodes"
+echo "Please share this programm and modify it for more functions."
+echo " - Minecodes"
+
+export alias cls="clear"
+git clone https://gitlab.com/minecodes-codes/PineapplePi.git
+echo "please edit the file atack"
+echo "Good luck and"
+echo "Happy hacking! ;)"
